f8b0bdd74e0a9035ea7eb69f32c59c4d7dc6c2c5
Ken Moriarty <kwatford@gmail.com>
2022-10-02 11:26:54 -0600

    Add geas5core as a submodule to make iterating easier.

--- tests/ruleset_loader_test.py
+++ tests/ruleset_loader_test.py
@@ -6,7 +6,12 @@ import pytest
 
 from camp.engine import loader
 
-RULESET = pathlib.Path(__file__).parent.parent.parent / "examples" / "geastest"
+BASEDIR = pathlib.Path(__file__).parent.parent
+PATH_PARAMS = [
+    pytest.param(BASEDIR / "examples" / "geastest", id="geastest"),
+    pytest.param(BASEDIR / "repos" / "geas5core" / "core", id="geas5core"),
+]
+
 FAIL_TEMPLATE = """
 Could not parse {path}
 
@@ -18,7 +23,8 @@ Parsed data:
 """
 
 
-def test_load_ruleset():
+@pytest.mark.parametrize("path", PATH_PARAMS)
+def test_load_ruleset(path):
     """Very basic loader test.
 
     1. Does the load return with no bad defs?
@@ -26,7 +32,7 @@ def test_load_ruleset():
 
     Other tests will cover more specific cases.
     """
-    ruleset = loader.load_ruleset(RULESET)
+    ruleset = loader.load_ruleset(path)
     if ruleset.bad_defs:
         bd = ruleset.bad_defs[0]
         if "description" in bd.data:
@@ -43,23 +49,25 @@ def test_load_ruleset():
     assert ruleset.features
 
 
+@pytest.mark.parametrize("path", PATH_PARAMS)
 @pytest.mark.parametrize("format", ["zip"])
-def test_load_archive_ruleset(tmp_path_factory, format):
+def test_load_archive_ruleset(path, format, tmp_path_factory):
     """Zipfile loader test."""
     temp_base = tmp_path_factory.mktemp("camp-engine-test") / "GEAS5CORE"
-    archive = shutil.make_archive(temp_base, format, root_dir=RULESET)
+    archive = shutil.make_archive(temp_base, format, root_dir=path)
     ruleset = loader.load_ruleset(archive)
     assert not ruleset.bad_defs
     assert ruleset.features
 
 
-def test_serialize_ruleset():
+@pytest.mark.parametrize("path", PATH_PARAMS)
+def test_serialize_ruleset(path):
     """Test that the ruleset can be serialized and deserialized.
 
     For this to work, the ruleset must properly indicate its
     feature types on its ruleset subclass.
     """
-    ruleset = loader.load_ruleset(RULESET)
+    ruleset = loader.load_ruleset(path)
     ruleset_json = ruleset.dump()
     assert ruleset_json
     reloaded_ruleset = loader.deserialize_ruleset(ruleset_json)
