136aa8b986739d5e8bce3250c606a3292d606f6f
Ken Moriarty <ken@moriarty.wtf>
2024-03-20 08:50:57 -0600

    Update pre-commit and tests

--- tests/tempest_test/conftest.py
+++ tests/tempest_test/conftest.py
@@ -1,22 +1,23 @@
 """Shared fixtures for Tempest engine tests."""
 
+import pathlib
 from typing import cast
 
 import pytest
-import pathlib
 
 from camp.engine import loader
 from camp.engine.rules.base_engine import Engine
 from camp.engine.rules.tempest.controllers.character_controller import TempestCharacter
 from camp.engine.rules.tempest.engine import TempestEngine
 
-
-RULESET_PATH = pathlib.Path(__file__).parent.parent.parent / "rulesets"
+RULESET_PATH = pathlib.Path(__file__).parent.parent / "rulesets"
 
 
 @pytest.fixture
 def engine() -> TempestEngine:
-    engine = cast(TempestEngine, loader.load_ruleset(RULESET_PATH / "tempest_test").engine)
+    engine = cast(
+        TempestEngine, loader.load_ruleset(RULESET_PATH / "tempest_test").engine
+    )
     assert engine.ruleset.bad_defs == []
     return engine
 
